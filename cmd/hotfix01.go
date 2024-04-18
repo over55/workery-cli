aba9e9690c86ebb8a96603baa1a74e1b77716677
Bartlomiej Mika <bart@mikasoftware.com>
2024-04-18 17:16:39 -0400

    Added into transactions

--- cmd/hotfix01.go
+++ cmd/hotfix01.go
@@ -82,6 +82,35 @@ func RunHotfix01(
 		log.Fatal(err)
 	}
 
+	for _, datum := range customerData {
+		if err := hotfix01Customer(mc, tenantStorer, userStorer, cStorer, hhStorer, tenant, datum); err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Finished importing associates")
+	for _, datum := range associateData {
+		if err := hotfix01Associate(mc, tenantStorer, userStorer, aStorer, hhStorer, tenant, datum); err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Finished importing associates")
+	for _, datum := range staffData {
+		if err := hotfix01Staff(mc, tenantStorer, userStorer, sStorer, hhStorer, tenant, datum); err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Println("Finished importing staffs")
+}
+
+func hotfix01Customer(
+	mc *mongo.Client,
+	ts tenant_ds.TenantStorer,
+	userStorer user_ds.UserStorer,
+	customerStorer c_ds.CustomerStorer,
+	hhStorer hh_ds.HowHearAboutUsItemStorer,
+	tenant *tenant_ds.Tenant,
+	oldCustomer *OldCustomer,
+) error {
 	////
 	//// Start the transaction.
 	////
@@ -94,24 +123,59 @@ func RunHotfix01(
 
 	// Define a transaction function with a series of operations
 	transactionFunc := func(sessCtx mongo.SessionContext) (interface{}, error) {
-		for _, datum := range customerData {
-			if err := hotfix01Customer(sessCtx, tenantStorer, userStorer, cStorer, hhStorer, tenant, datum); err != nil {
-				return nil, err
-			}
+		newCustomer, err := customerStorer.GetByEmail(sessCtx, oldCustomer.Email.ValueOrZero())
+		if err != nil {
+			return nil, err
 		}
-		fmt.Println("Finished importing associates")
-		for _, datum := range associateData {
-			if err := hotfix01Associate(sessCtx, tenantStorer, userStorer, aStorer, hhStorer, tenant, datum); err != nil {
-				return nil, err
-			}
+		if newCustomer == nil {
+			err := fmt.Errorf("does not exist for Email: %v", oldCustomer.Email.ValueOrZero())
+			log.Println("err:", err)
+			return nil, err
+			// return err
 		}
-		fmt.Println("Finished importing associates")
-		for _, datum := range staffData {
-			if err := hotfix01Staff(sessCtx, tenantStorer, userStorer, sStorer, hhStorer, tenant, datum); err != nil {
-				return nil, err
-			}
+
+		//
+		// Fix 1 - Make sure correct public id is set.
+		//
+
+		newCustomer.PublicID = oldCustomer.ID
+
+		// // for debugging purposes.
+		// fmt.Println(newCustomer.PublicID, oldCustomer.ID, "--->", newCustomer.Email)
+
+		// Save fix 1.
+		if err := customerStorer.UpdateByID(sessCtx, newCustomer); err != nil {
+			return nil, err
 		}
-		fmt.Println("Finished importing staffs")
+
+		//
+		// Fix 2 - Modified by
+		//
+
+		modifiedByCustomer, err := userStorer.GetByPublicID(sessCtx, uint64(oldCustomer.LastModifiedById.ValueOrZero()))
+		if err != nil {
+			return nil, err
+		}
+		if modifiedByCustomer == nil {
+			err := fmt.Errorf("does not exist for LastModifiedById: %v", oldCustomer.LastModifiedById.ValueOrZero())
+			log.Println("err:", err)
+			return nil, err
+			// return err
+		}
+
+		newCustomer.ModifiedByUserID = modifiedByCustomer.ID
+		newCustomer.ModifiedByUserName = modifiedByCustomer.Name
+
+		// fmt.Println("newCustomer.ModifiedByUserID -->", newCustomer.ID)
+		// fmt.Println(newCustomer.ID, newCustomer.Name, "-->", newCustomer.ModifiedByUserName)
+		// fmt.Println()
+
+		// Save fix 2.
+		if err := customerStorer.UpdateByID(sessCtx, newCustomer); err != nil {
+			return nil, err
+		}
+
+		fmt.Println("hotfixed customer:", newCustomer.PublicID)
 		return nil, nil
 	}
 
@@ -119,74 +183,11 @@ func RunHotfix01(
 	if _, err := session.WithTransaction(context.Background(), transactionFunc); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func hotfix01Customer(
-	ctx context.Context,
-	ts tenant_ds.TenantStorer,
-	userStorer user_ds.UserStorer,
-	customerStorer c_ds.CustomerStorer,
-	hhStorer hh_ds.HowHearAboutUsItemStorer,
-	tenant *tenant_ds.Tenant,
-	oldCustomer *OldCustomer,
-) error {
-	newCustomer, err := customerStorer.GetByEmail(ctx, oldCustomer.Email.ValueOrZero())
-	if err != nil {
-		return err
-	}
-	if newCustomer == nil {
-		err := fmt.Errorf("does not exist for Email: %v", oldCustomer.Email.ValueOrZero())
-		log.Println("err:", err)
-		return nil
-		// return err
-	}
-
-	//
-	// Fix 1 - Make sure correct public id is set.
-	//
-
-	newCustomer.PublicID = oldCustomer.ID
-
-	// // for debugging purposes.
-	// fmt.Println(newCustomer.PublicID, oldCustomer.ID, "--->", newCustomer.Email)
-
-	// Save fix 1.
-	if err := customerStorer.UpdateByID(ctx, newCustomer); err != nil {
-		return err
-	}
-
-	//
-	// Fix 2 - Modified by
-	//
-
-	modifiedByCustomer, err := userStorer.GetByPublicID(ctx, uint64(oldCustomer.LastModifiedById.ValueOrZero()))
-	if err != nil {
-		return err
-	}
-	if modifiedByCustomer == nil {
-		err := fmt.Errorf("does not exist for LastModifiedById: %v", oldCustomer.LastModifiedById.ValueOrZero())
-		log.Println("err:", err)
-		return nil
-		// return err
-	}
-
-	newCustomer.ModifiedByUserID = modifiedByCustomer.ID
-	newCustomer.ModifiedByUserName = modifiedByCustomer.Name
-
-	// fmt.Println("newCustomer.ModifiedByUserID -->", newCustomer.ID)
-	// fmt.Println(newCustomer.ID, newCustomer.Name, "-->", newCustomer.ModifiedByUserName)
-	// fmt.Println()
-
-	// Save fix 2.
-	if err := customerStorer.UpdateByID(ctx, newCustomer); err != nil {
-		return err
-	}
-
 	return nil
 }
 
 func hotfix01Associate(
-	ctx context.Context,
+	mc *mongo.Client,
 	ts tenant_ds.TenantStorer,
 	userStorer user_ds.UserStorer,
 	associateStorer a_ds.AssociateStorer,
@@ -194,63 +195,81 @@ func hotfix01Associate(
 	tenant *tenant_ds.Tenant,
 	oldAssociate *OldAssociate,
 ) error {
-	newAssociate, err := associateStorer.GetByEmail(ctx, oldAssociate.Email.ValueOrZero())
+	////
+	//// Start the transaction.
+	////
+
+	session, err := mc.StartSession()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	if newAssociate == nil {
-		err := fmt.Errorf("does not exist for Email: %v", oldAssociate.Email.ValueOrZero())
-		log.Println("err:", err)
-		return nil
-		// return err
+	defer session.EndSession(context.Background())
+
+	// Define a transaction function with a series of operations
+	transactionFunc := func(sessCtx mongo.SessionContext) (interface{}, error) {
+		newAssociate, err := associateStorer.GetByEmail(sessCtx, oldAssociate.Email.ValueOrZero())
+		if err != nil {
+			return nil, err
+		}
+		if newAssociate == nil {
+			err := fmt.Errorf("does not exist for Email: %v", oldAssociate.Email.ValueOrZero())
+			log.Println("err:", err)
+			return nil, err
+		}
+
+		//
+		// Fix 1 - Make sure correct public id is set.
+		//
+
+		newAssociate.PublicID = oldAssociate.ID
+
+		// // for debugging purposes.
+		// fmt.Println(newAssociate.PublicID, oldAssociate.ID, "--->", newAssociate.Email)
+
+		// Save fix 1.
+		if err := associateStorer.UpdateByID(sessCtx, newAssociate); err != nil {
+			return nil, err
+		}
+
+		//
+		// Fix 2 - Modified by
+		//
+
+		modifiedByAssociate, err := userStorer.GetByPublicID(sessCtx, uint64(oldAssociate.LastModifiedByID.ValueOrZero()))
+		if err != nil {
+			return nil, err
+		}
+		if modifiedByAssociate == nil {
+			err := fmt.Errorf("does not exist for LastModifiedByID: %v", oldAssociate.LastModifiedByID.ValueOrZero())
+			log.Println("err:", err)
+			return nil, err
+		}
+
+		newAssociate.ModifiedByUserID = modifiedByAssociate.ID
+		newAssociate.ModifiedByUserName = modifiedByAssociate.Name
+
+		// fmt.Println("newAssociate.ModifiedByUserID -->", newAssociate.ID)
+		// fmt.Println(newAssociate.ID, newAssociate.Name, "-->", newAssociate.ModifiedByUserName)
+		// fmt.Println()
+
+		// Save fix 2.
+		if err := associateStorer.UpdateByID(sessCtx, newAssociate); err != nil {
+			return nil, err
+		}
+
+		fmt.Println("hotfixed associate:", newAssociate.PublicID)
+		return nil, nil
 	}
 
-	//
-	// Fix 1 - Make sure correct public id is set.
-	//
-
-	newAssociate.PublicID = oldAssociate.ID
-
-	// // for debugging purposes.
-	// fmt.Println(newAssociate.PublicID, oldAssociate.ID, "--->", newAssociate.Email)
-
-	// Save fix 1.
-	if err := associateStorer.UpdateByID(ctx, newAssociate); err != nil {
-		return err
+	// Start a transaction
+	if _, err := session.WithTransaction(context.Background(), transactionFunc); err != nil {
+		log.Fatal(err)
 	}
-
-	//
-	// Fix 2 - Modified by
-	//
-
-	modifiedByAssociate, err := userStorer.GetByPublicID(ctx, uint64(oldAssociate.LastModifiedByID.ValueOrZero()))
-	if err != nil {
-		return err
-	}
-	if modifiedByAssociate == nil {
-		err := fmt.Errorf("does not exist for LastModifiedByID: %v", oldAssociate.LastModifiedByID.ValueOrZero())
-		log.Println("err:", err)
-		return nil
-		// return err
-	}
-
-	newAssociate.ModifiedByUserID = modifiedByAssociate.ID
-	newAssociate.ModifiedByUserName = modifiedByAssociate.Name
-
-	// fmt.Println("newAssociate.ModifiedByUserID -->", newAssociate.ID)
-	// fmt.Println(newAssociate.ID, newAssociate.Name, "-->", newAssociate.ModifiedByUserName)
-	// fmt.Println()
-
-	// Save fix 2.
-	if err := associateStorer.UpdateByID(ctx, newAssociate); err != nil {
-		return err
-	}
-
 	return nil
 }
 
 func hotfix01Staff(
-	ctx context.Context,
+	mc *mongo.Client,
 	ts tenant_ds.TenantStorer,
 	userStorer user_ds.UserStorer,
 	staffStorer s_ds.StaffStorer,
@@ -258,57 +277,75 @@ func hotfix01Staff(
 	tenant *tenant_ds.Tenant,
 	oldStaff *OldStaff,
 ) error {
-	newStaff, err := staffStorer.GetByEmail(ctx, oldStaff.Email.ValueOrZero())
+	////
+	//// Start the transaction.
+	////
+
+	session, err := mc.StartSession()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	if newStaff == nil {
-		err := fmt.Errorf("does not exist for Email: %v", oldStaff.Email.ValueOrZero())
-		log.Println("err:", err)
-		return nil
-		// return err
+	defer session.EndSession(context.Background())
+
+	// Define a transaction function with a series of operations
+	transactionFunc := func(sessCtx mongo.SessionContext) (interface{}, error) {
+		newStaff, err := staffStorer.GetByEmail(sessCtx, oldStaff.Email.ValueOrZero())
+		if err != nil {
+			return nil, err
+		}
+		if newStaff == nil {
+			err := fmt.Errorf("does not exist for Email: %v", oldStaff.Email.ValueOrZero())
+			log.Println("err:", err)
+			return nil, err
+		}
+
+		//
+		// Fix 1 - Make sure correct public id is set.
+		//
+
+		newStaff.PublicID = oldStaff.ID
+
+		// // for debugging purposes.
+		// fmt.Println(newStaff.PublicID, oldStaff.ID, "--->", newStaff.Email)
+
+		// Save fix 1.
+		if err := staffStorer.UpdateByID(sessCtx, newStaff); err != nil {
+			return nil, err
+		}
+
+		//
+		// Fix 2 - Modified by
+		//
+
+		modifiedByStaff, err := userStorer.GetByPublicID(sessCtx, uint64(oldStaff.LastModifiedByID.ValueOrZero()))
+		if err != nil {
+			return nil, err
+		}
+		if modifiedByStaff == nil {
+			err := fmt.Errorf("does not exist for LastModifiedByID: %v", oldStaff.LastModifiedByID.ValueOrZero())
+			log.Println("err:", err)
+			return nil, err
+		}
+
+		newStaff.ModifiedByUserID = modifiedByStaff.ID
+		newStaff.ModifiedByUserName = modifiedByStaff.Name
+
+		// fmt.Println("newStaff.ModifiedByUserID -->", newStaff.ID)
+		// fmt.Println(newStaff.ID, newStaff.Name, "-->", newStaff.ModifiedByUserName)
+		// fmt.Println()
+
+		// Save fix 2.
+		if err := staffStorer.UpdateByID(sessCtx, newStaff); err != nil {
+			return nil, err
+		}
+
+		fmt.Println("hotfixed staff:", newStaff.PublicID)
+		return nil, nil
 	}
 
-	//
-	// Fix 1 - Make sure correct public id is set.
-	//
-
-	newStaff.PublicID = oldStaff.ID
-
-	// // for debugging purposes.
-	// fmt.Println(newStaff.PublicID, oldStaff.ID, "--->", newStaff.Email)
-
-	// Save fix 1.
-	if err := staffStorer.UpdateByID(ctx, newStaff); err != nil {
-		return err
+	// Start a transaction
+	if _, err := session.WithTransaction(context.Background(), transactionFunc); err != nil {
+		log.Fatal(err)
 	}
-
-	//
-	// Fix 2 - Modified by
-	//
-
-	modifiedByStaff, err := userStorer.GetByPublicID(ctx, uint64(oldStaff.LastModifiedByID.ValueOrZero()))
-	if err != nil {
-		return err
-	}
-	if modifiedByStaff == nil {
-		err := fmt.Errorf("does not exist for LastModifiedByID: %v", oldStaff.LastModifiedByID.ValueOrZero())
-		log.Println("err:", err)
-		return nil
-		// return err
-	}
-
-	newStaff.ModifiedByUserID = modifiedByStaff.ID
-	newStaff.ModifiedByUserName = modifiedByStaff.Name
-
-	// fmt.Println("newStaff.ModifiedByUserID -->", newStaff.ID)
-	// fmt.Println(newStaff.ID, newStaff.Name, "-->", newStaff.ModifiedByUserName)
-	// fmt.Println()
-
-	// Save fix 2.
-	if err := staffStorer.UpdateByID(ctx, newStaff); err != nil {
-		return err
-	}
-
 	return nil
 }
