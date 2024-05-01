256a893180eaf25a40e41035de80de15d66ebf48
Bartlomiej Mika <bart@mikasoftware.com>
2024-05-01 01:13:44 -0400

    Added useful comments

--- cmd/hotfix05.go
+++ cmd/hotfix05.go
@@ -70,36 +70,48 @@ func RunHotfix05(
 
 	// Define a transaction function with a series of operations
 	transactionFunc := func(sessCtx mongo.SessionContext) (interface{}, error) {
+		log.Println("fetching all associates...")
 		aa, err := aStorer.ListAll(context.Background())
 		if err != nil {
 			return nil, err
 		}
+		log.Printf("iterating through %v associates...\n", len(aa.Results))
 		for _, a := range aa.Results {
+			// log.Println("fetching all orders by associate...")
 			oo, err := oStorer.ListByAssociateID(context.Background(), a.ID)
 			if err != nil {
+				log.Println("error:", err)
 				return nil, err
 			}
-			for _, o := range oo.Results {
-				log.Println("A_ID", a.ID, "| O_ID", o.ID)
-				o.AssociateServiceFeeID = a.ServiceFeeID
-				o.AssociateServiceFeeName = a.ServiceFeeName
-				o.AssociateServiceFeePercentage = a.ServiceFeePercentage
-				if err := oStorer.UpdateByID(context.Background(), o); err != nil {
-					return nil, err
+			if len(oo.Results) > 0 {
+				log.Printf("iterating through %v orders...", len(oo.Results))
+				for _, o := range oo.Results {
+					log.Println("A_ID", a.ID, "| O_ID", o.ID)
+					o.AssociateServiceFeeID = a.ServiceFeeID
+					o.AssociateServiceFeeName = a.ServiceFeeName
+					o.AssociateServiceFeePercentage = a.ServiceFeePercentage
+					if err := oStorer.UpdateByID(context.Background(), o); err != nil {
+						return nil, err
+					}
 				}
 			}
 
+			// log.Println("fetching all task items by associate...")
 			titi, err := tiStorer.ListByAssociateID(context.Background(), a.ID)
 			if err != nil {
 				return nil, err
 			}
-			for _, ti := range titi.Results {
-				log.Println("A_ID", a.ID, "| TI_ID", ti.ID)
-				ti.AssociateServiceFeeID = a.ServiceFeeID
-				ti.AssociateServiceFeeName = a.ServiceFeeName
-				ti.AssociateServiceFeePercentage = a.ServiceFeePercentage
-				if err := tiStorer.UpdateByID(context.Background(), ti); err != nil {
-					return nil, err
+
+			if len(titi.Results) > 0 {
+				log.Printf("iterating through %v task items...", len(titi.Results))
+				for _, ti := range titi.Results {
+					log.Println("A_ID", a.ID, "| TI_ID", ti.ID)
+					ti.AssociateServiceFeeID = a.ServiceFeeID
+					ti.AssociateServiceFeeName = a.ServiceFeeName
+					ti.AssociateServiceFeePercentage = a.ServiceFeePercentage
+					if err := tiStorer.UpdateByID(context.Background(), ti); err != nil {
+						return nil, err
+					}
 				}
 			}
 
